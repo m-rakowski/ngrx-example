9a3c8caa942c8586c9b1d850d5ab9dc35cc22bd0
m-rakowski <michalrakowski@yahoo.com>
2020-09-16 00:34:40 +0200

    attach photos to posts

--- src/app/posts/model/post.ts
+++ src/app/posts/model/post.ts
@@ -1,7 +1,12 @@
 import { firestore } from 'firebase';
 export interface Post {
   id: string;
-  title: string;
-  content: string;
   created?: firestore.Timestamp;
+  question: string;
+  title1: string;
+  title2: string;
+  description1: string;
+  description2: string;
+  photo1Url: string;
+  photo2Url: string;
 }
--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -5,11 +5,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { PostsState } from '../../store/reducers';
 import { selectIsLoading, selectPostsAsArray } from '../../store/selectors';
-import {
-  actionCreatePost,
-  actionDeleteLastPost,
-  actionGetAllPosts,
-} from '../../store/actions';
+import { actionCreatePost, actionDeleteLastPost, actionGetAllPosts } from '../../store/actions';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-posts-list',
@@ -20,8 +17,13 @@ export class PostsListComponent implements OnInit {
   posts$: Observable<Post[]>;
   isLoading$: Observable<boolean>;
   formGroup: FormGroup;
+  task: AngularFireUploadTask;
+  uploadProgressPhoto1$: Observable<number | undefined>;
+  uploadProgressPhoto2$: Observable<number | undefined>;
+  private photo1Id: string;
+  private photo2Id: string;
 
-  constructor(private store: Store<PostsState>) {}
+  constructor(private store: Store<PostsState>, private readonly angularFireStorage: AngularFireStorage) {}
 
   ngOnInit(): void {
     this.createForm();
@@ -36,12 +38,25 @@ export class PostsListComponent implements OnInit {
     }
   }
 
-  getAllPosts() {
-    this.store.dispatch(actionGetAllPosts());
+  async uploadPhoto1(event) {
+    this.photo1Id = Math.random().toString(36).substring(2);
+    const angularFireUploadTask = await this.angularFireStorage.upload(this.photo1Id, event.target.files[0]);
+    console.log('angularFireUploadTask', angularFireUploadTask);
+    const downloadURL = await angularFireUploadTask.ref.getDownloadURL();
+    console.log('downloadURL', downloadURL);
+
+    this.formGroup.get('photo1Url').patchValue(downloadURL);
   }
 
-  formControl(controlName: string): FormControl {
-    return this.formGroup.get(controlName) as FormControl;
+  async uploadPhoto2(event) {
+    this.photo2Id = Math.random().toString(36).substring(2);
+    const angularFireUploadTask = await this.angularFireStorage.upload(this.photo2Id, event.target.files[0]);
+    const downloadURL = await angularFireUploadTask.ref.getDownloadURL();
+    this.formGroup.get('photo2Url').patchValue(downloadURL);
+  }
+
+  getAllPosts() {
+    this.store.dispatch(actionGetAllPosts());
   }
 
   deleteLastPost() {
@@ -50,8 +65,13 @@ export class PostsListComponent implements OnInit {
 
   private createForm(): void {
     this.formGroup = new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      content: new FormControl('', [Validators.required]),
+      question: new FormControl('', [Validators.required]),
+      title1: new FormControl('', [Validators.required]),
+      title2: new FormControl('', [Validators.required]),
+      description1: new FormControl('', [Validators.required]),
+      description2: new FormControl('', [Validators.required]),
+      photo1Url: new FormControl('', [Validators.required]),
+      photo2Url: new FormControl('', [Validators.required]),
     });
   }
 }
