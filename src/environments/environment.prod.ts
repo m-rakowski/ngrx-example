b0e3323a791d5787556e8daf470766e2e3449c2d
m-rakowski <michalrakowski@yahoo.com>
2020-09-16 02:00:25 +0200

    firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDaShPqQFdE8kGzHw29rJAZ4XmcScboRt8',
+    authDomain: 'this-or-that-b045e.firebaseapp.com',
+    databaseURL: 'https://this-or-that-b045e.firebaseio.com',
+    projectId: 'this-or-that-b045e',
+    storageBucket: 'this-or-that-b045e.appspot.com',
+    messagingSenderId: '195463027271',
+    appId: '1:195463027271:web:2f7d0565dcd11806696f62',
+  },
 };
